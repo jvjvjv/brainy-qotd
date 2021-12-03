c8cc2f39e58281b37127131a6fa2c3ae35fafd50
Jason Vertucio <me@jasonvertucio.com>
2021-12-03 08:37:44 -0500

    Select random QOTD

--- index.js
+++ index.js
@@ -27,16 +27,20 @@ async function run () {
       height: 630
     });
 
+    // Load page and get random quote;
     await page.goto(url, waitOptions);
-    const todaysQuoteLink = await page.evaluate(`document.querySelector('a[title="view quote"]').getAttribute('href')`);
+    const todaysQuoteLink = await page.evaluate(`a=document.querySelectorAll('.qotd-wrapper-cntr a[href*="/quotes/"]'),b=a.length;c=Math.floor(Math.random()*b),a[c].getAttribute('href')`);
 
+    // Go to random quote and select a random image
     await page.goto(`https://brainyquote.com/${todaysQuoteLink}`, waitOptions);
     const finalImageLink = await page.evaluate(`a=document.querySelectorAll(".quoteDetailThumbs a"),b=a.length,c=Math.floor(Math.random()*b),a[c].getAttribute('href')`);
 
+    // Load the random image page and fetch image link
     await page.goto(`https://brainyquote.com/${finalImageLink}`, waitOptions);
     const imgSrc = await page.evaluate(`document.querySelector('.qti-listm img').getAttribute('src');`);
     const img = await page.goto(`https://brainyquote.com/${imgSrc}`);
 
+    // Download image link
     writeFile(filename, await img.buffer(), function (err) {
       if (err) return console.log(err);
       return console.log(`File '${filename}' has left the library. File '${filename}' has been saved.`)
